ae6b05cf7c2d6c6c923aa5424364c5d9fd6c48ae
PainSteff <steffinjoseph90@gmail.com>
2017-01-24 19:45:46 +0530

    Update HomeController.java

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/")
 public class HomeController {
 
-	@RequestMapping(value  = "/home", , method = RequestMethod.GET)
+	@RequestMapping(value  = "/home", method = RequestMethod.GET)
 	public String hello() {
 		return "This is the first spring boot application";
 	}
